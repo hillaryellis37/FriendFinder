6b565f8f8dc7cc35246005c529cb58f6cb921a94
Live Sound Systems <hillaryellis37@gmail.com>
2017-10-17 00:20:13 -0700

    got server to recognize public file

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+	{
+		"name": "Hillary",
+		"photo": "https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/389_100826970042_8416_n.jpg?oh=dd8f075d1377cca45500c895a137024e&oe=5A81C69F",
+		"scores": [1,1,1,1,1,1,1,1,1,1]
+	}
+];
+
+module.exports = friends;
