7c32a3baba4c5239579654d8dbd8e1a807158d5f
Live Sound Systems <hillaryellis37@gmail.com>
2017-10-20 13:09:32 -0700

    fixed required data to not pop modal if no input exists

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -2,8 +2,6 @@
 var survey = [];
 var soulmateName;
 var soulmateImg; 
-// var quotient = 40;
-// var smallestSum = 0;
 var currentScore;
 var smallestDif = 40;
 
@@ -11,101 +9,83 @@ function soulmateLogic(scores) {
 
   var dif = 0;
 
+  for(var i = 0; i < scores.length; i++) {
 
-      for(var i = 0; i < scores.length; i++) {
-
-        var difference = Math.abs(parseInt(scores[i]) - currentScore[i]);
-        dif += difference;
-
-      }
-
-      // console.log("quotient:", quotient);
-      // console.log("smallestSum:", smallestDif);
-      // console.log(smallestSum < quotient);
-
+    var difference = Math.abs(parseInt(scores[i]) - currentScore[i]);
+    dif += difference;
+  }
       
-      if (dif < smallestDif) {
-        smallestDif = dif;
-
-        console.log("smallest Dif:", smallestDif);
-        console.log("current dif:", dif);
-        return true;
-
-      //     soulmateName = data[i].name;
-      //     soulmateImg = data[i].photo;
-      //     console.log(soulmateName);
-      //     console.log(soulmateImg);
-      //     console.log("quotient:", quotient);
-      //     console.log("smallest sun", smallestSum);
+    if (dif < smallestDif) {
+      smallestDif = dif;
+      return true;      
+      
       } 
 }
 
-
-//Switcher function:
 $(".rb-tab").click(function(){
-  //Spot switcher:
   $(this).parent().find(".rb-tab").removeClass("rb-tab-active");
   $(this).addClass("rb-tab-active");
 });
 
 
 $(".trigger").on("click", function(event){
-  smallestDif = 40
-  event.preventDefault(); 
 
-  survey = [];
+  userInputName = $("#new-user").val().trim();
+  userInputURL = $("#url").val().trim();
 
-  for (i=1; i<=$(".rb").length; i++) {
-    var rb = "rb" + i;
-    var rbValue = parseInt($("#rb-"+i).find(".rb-tab-active").attr("data-value"));
-    survey.push(rbValue); 
-  };
+  if ((userInputName != "") && (userInputURL != "")) {
+    smallestDif = 40
+    event.preventDefault(); 
 
+    survey = [];
 
-  var newFriend = {
-    name: $("#new-user").val().trim(),
-    photo: $("#url").val().trim(),
-    scores: survey
-  };
-
- 
-  $.post("/api/friends/", newFriend, function(data) {
-  });
-
-  $.get("/api/friends/", function(data) {
+    for (i=1; i<=$(".rb").length; i++) {
+      var rb = "rb" + i;
+      var rbValue = parseInt($("#rb-"+i).find(".rb-tab-active").attr("data-value"));
+      survey.push(rbValue); 
+    };
 
 
-    currentScore = newFriend.scores;   
-    
-    console.log("current score", currentScore);
-    console.log("data length:", data.length);
-    
-    
-    for (var i = 0; i < data.length - 1; i++) {
+    var newFriend = {
+      name: $("#new-user").val().trim(),
+      photo: $("#url").val().trim(),
+      scores: survey
+    };
 
-    var scores = data[i].scores; 
+   
+    $.post("/api/friends/", newFriend, function(data) {
+    });
+
+    $.get("/api/friends/", function(data) {
 
 
-    console.log("score:", scores);
+      currentScore = newFriend.scores;   
+      
+      console.log("current score", currentScore);
+      console.log("data length:", data.length);
+      
+      
+      for (var i = 0; i < data.length - 1; i++) {
 
-      if (soulmateLogic(scores)) {
+      var scores = data[i].scores; 
 
+
+      console.log("score:", scores);
+
+        if (soulmateLogic(scores)) {
           
-          
-          soulmateName = data[i].name;
-          soulmateImg = data[i].photo;
-          console.log(soulmateName);
-          console.log(soulmateImg);
-          // console.log("quotient:", quotient);
-          // console.log("smallest sun", smallestSum);
-      } 
+            soulmateName = data[i].name;
+            soulmateImg = data[i].photo;
+        } 
 
-     }
+       }
 
-    $("#sm-name").html(soulmateName);
-    $("#sm-img").attr("src", soulmateImg);
+      $("#sm-name").html(soulmateName);
+      $("#sm-img").attr("src", soulmateImg);
+      $('#exampleModal').modal('show');
 
 
-  });
+    });
+  }
 
 });
